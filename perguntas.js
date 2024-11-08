4e91764e4078210887fb77532ebc48e2eade66cd
Cinthiajasminn <00001117358811sp@al.educacao.sp.gov.br>
2024-11-09 02:17:22 +1100

    C

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'mundo do trabalho',
+    'O que é hardskills e softskills?',
+    'Hardskils são habilidades técnicas e softskills são habilidades comportâmentais'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
